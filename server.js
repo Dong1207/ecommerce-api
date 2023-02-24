67779fc5420bb7aaced6323a7aa98eaf6479ac2d
Dong NV <chomottinhyeu127@gmail.com>
2023-02-24 09:36:33 +0700

    init repo

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3003;
+const syncServer = require('./helper/getIp')
+
+syncServer.syncToCloudflare();
 
 app.get('/', (req, res) => {
-  res.send('Hello World1!')
-})
+  res.send('Hello World Dong!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
